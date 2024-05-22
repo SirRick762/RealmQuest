ab8e3f631ba9a5772815adb48f1651334a2aa43a
SirRick762 <131669709+SirRick762@users.noreply.github.com>
2024-05-22 15:54:40 +0100

    Wander + Nivel 2/3

---
+++ Assets/_Project/Scripts/NPC Wonder/AIControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Plataformer
+{
+    public class AIControl : MonoBehaviour
+    {
+        GameObject[] goalLocations;
+        NavMeshAgent agent;
+        Animator anim;
+
+        void Start()
+        {
+
+            agent = GetComponent<NavMeshAgent>();
+            goalLocations = GameObject.FindGameObjectsWithTag("goal");
+            int i = Random.Range(0, goalLocations.Length);
+            agent.SetDestination(goalLocations[i].transform.position);
+            anim = this.GetComponent<Animator>();
+            anim.SetTrigger("isWalking");
+            anim.SetFloat("wOffset", Random.Range(0.0f, 1.0f));
+            float sm = Random.Range(0.5f, 2.0f);
+            anim.SetFloat("speedMult", sm);
+            agent.speed *= sm;// como demos speed a anima��o tambem temos que o fazer para o agent
+
+        }
+
+
+        void Update()
+        {
+
+            if (agent.remainingDistance < 1.0f)
+            {
+
+                int i = Random.Range(0, goalLocations.Length);
+                agent.SetDestination(goalLocations[i].transform.position);
+            }
+        }
+    }
+}
